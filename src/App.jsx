222a1e3ecb80709641be253d3833f40f792623b8
Andrey Lavrjonov <kedr_club@mail.ru>
2023-03-01 18:39:50 +0300

    feat: adds player

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from './components/Form/Form';
+import Player from './components/Player/Player';
+
+function App() {
+  return (
+    // <Form />
+    <Player />
+  )
+}
+
+export default App;
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useRef, useState } from 'react';
 import style from './Player.module.css';
 
 const Player = () => {
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const audioRef = useRef(null);
+
+  // useEffect(() => {
+  //   console.log(audioRef)
+  //   if (isPlaying) {
+  //     audioRef.current.play();
+  //   } else {
+  //     audioRef.current.pause();
+  //   }
+  // }, [isPlaying]);
 
   return (
-    <>
-      <div className={style.player}></div>
-    </>
+      <div className={style.player}>
+        <audio controls>
+          <source src="./128.mp3" />
+        </audio>
+      </div>
   )
 }
 
